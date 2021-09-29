8a420b9ed589acf5ce3bf1096576d3dd604c3e2c
pauloranye <ora21001@byui.edu>
2021-09-29 14:51:56 +0100

    My first commit!

---
+++ Solo projects/practice.py
@@ -0,0 +1,23 @@
+class calculator(object):
+
+
+    def addition(x,y):
+        add = x + y
+        print(add)
+
+    def subtraction(x,y):
+        sub = x - y
+        print(sub)
+
+    def multiplication(x,y):
+        mult = x * y
+        print(mult)
+    
+    def division(x,y):
+        div = x/ y
+        print(div)
+
+calculator.addition(2,3)
+calculator.multiplication(5,6)
+    
+        
